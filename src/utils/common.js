a238a9e6801315f200b2f882dcb9f548cead43e0
ilianameit <ilianameit@gmail.com>
2023-06-05 23:29:08 +0300

    удаление ненужной функции

--- src/utils/common.js
+++ src/utils/common.js
@@ -10,13 +10,8 @@ function upperFirstCase(word){
   return (word[0].toUpperCase() + word.slice(1));
 }
 
-function updateItem(items, update) {
-  return items.map((item) => item.id === update.id ? update : item);
-}
-
 export {
   getRandomArrayElement,
   getRandomPrice,
   upperFirstCase,
-  updateItem
 };
