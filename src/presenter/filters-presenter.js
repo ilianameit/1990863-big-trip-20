461a9ca81b2ed18e6d9a51cb1c6ab4e887ff2a50
ilianameit <ilianameit@gmail.com>
2023-06-07 23:34:33 +0300

    методы для отрисовки вью

--- src/presenter/filters-presenter.js
+++ src/presenter/filters-presenter.js
@@ -1,16 +1,60 @@
+import { render, replace, remove } from '../framework/render.js';
 import FilterView from '../view/filters-view.js';
-import { render } from '../framework/render.js';
+import { filter } from '../utils/filter.js';
+import { FilterType, UpdateType } from '../const.js';
 
 export default class FiltersPresenter {
   #filterContainer = null;
-  #filters = null;
+  #filterModel = null;
+  #pointsModel = null;
 
-  constructor({filterContainer, filters}) {
+  #filterComponent = null;
+
+  constructor({filterContainer, filterModel, pointsModel}) {
     this.#filterContainer = filterContainer;
-    this.#filters = filters;
+    this.#filterModel = filterModel;
+    this.#pointsModel = pointsModel;
+
+    this.#pointsModel.addObserver(this.#handleModelEvent);
+    //this.#filterModel.addObserver(this.#handleModelEvent);
+  }
+
+  get filters() {
+    const points = this.#pointsModel.points;
+
+    return Object.values(FilterType).map((type) => ({
+      type,
+      count: filter[type](points).length
+    }));
   }
 
   init() {
-    render(new FilterView({filters: this.#filters}), this.#filterContainer);
+    const filters = this.filters;
+    const prevFilterComponent = this.#filterComponent;
+    this.#filterComponent = new FilterView({
+      filters,
+      currentFilterType: this.#filterModel.filter,
+      onFilterTypeChange: this.#handleFilterTypeChange
+    });
+
+    if (prevFilterComponent === null) {
+      render(this.#filterComponent, this.#filterContainer);
+      return;
+    }
+
+    replace(this.#filterComponent, prevFilterComponent);
+    remove(prevFilterComponent);
   }
+
+  #handleModelEvent = () => {
+    this.init();
+  };
+
+  #handleFilterTypeChange = (filterType) => {
+    if (this.#filterModel.filter === filterType) {
+      return;
+    }
+
+    this.#filterModel.setFilter(UpdateType.MAJOR, filterType);
+  };
 }
