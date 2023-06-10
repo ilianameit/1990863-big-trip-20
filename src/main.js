865a73f5f08d22ecb151993c1f51bfc72b2b4be0
ilianameit <ilianameit@gmail.com>
2023-06-10 19:00:55 +0300

    добавление новой точки

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
+import {RenderPosition, render} from './framework/render.js';
 import PointModel from './model/points-model.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import FiltersPresenter from './presenter/filters-presenter.js';
 import TripPresenter from './presenter/trip-presenter.js';
 import FilterModel from './model/filter-model.js';
+import NewPointButtonView from './view/new-point-button-view.js';
 
 const siteMainElement = document.querySelector('.page-body');
 const siteHeaderTripElement = siteMainElement.querySelector('.trip-main');
@@ -13,7 +15,22 @@ const pointsModel = new PointModel();
 const filterModel = new FilterModel();
 const tripPresenter = new TripPresenter({tripContainer: siteHeaderTripElement, pointsModel});
 const filterPresenter = new FiltersPresenter({filterContainer: siteHeaderFilterElement, filterModel, pointsModel});
-const boardPresenter = new BoardPresenter({listContainer: siteBodyElement, pointsModel, filterModel});
+const boardPresenter = new BoardPresenter({
+  listContainer: siteBodyElement,
+  pointsModel,
+  filterModel,
+  onNewPointDestroy: handleNewPointFormClose
+});
+
+const newPointButtonComponent = new NewPointButtonView({onClick: handleNewPointButtonClick});
+function handleNewPointFormClose() {
+  newPointButtonComponent.element.disabled = false;
+}
+function handleNewPointButtonClick () {
+  boardPresenter.createPoint();
+  newPointButtonComponent.element.disabled = true;
+}
+render(newPointButtonComponent, siteHeaderTripElement, RenderPosition.BEFOREEND);
 
 tripPresenter.init();
 filterPresenter.init();
