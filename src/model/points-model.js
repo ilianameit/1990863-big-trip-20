80056499b362cb77e597b4a7369e02e5792f2f41
ilianameit <ilianameit@gmail.com>
2023-06-12 20:22:47 +0300

    загрузка на сервер данных
    сначала обновится серверные данные. а затем и копия

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -32,6 +32,7 @@ export default class PointModel extends Observable {
       this.#destinations = await this.#pointsApiService.destinations;
 
       this.#offers = await this.#pointsApiService.offers;
+      console.log(this.#offers)
 
 
     } catch(err) {
@@ -42,20 +43,26 @@ export default class PointModel extends Observable {
     this._notify(UpdateType.INIT);
   }
 
-  updatePoint(updateType, update) {
+  async updatePoint(updateType, update) {
     const index = this.#points.findIndex((point) => point.id === update.id);
 
     if (index === -1) {
       throw new Error('Can\'t update unexisting point');
     }
 
-    this.#points = [
-      ...this.#points.slice(0, index),
-      update,
-      ...this.#points.slice(index + 1),
-    ];
+    try {
+      const response = await this.#pointsApiService.updatePoint(update);
+      const updatePoint = this.#adaptToClient(response);
+      this.#points = [
+        ...this.#points.slice(0, index),
+        updatePoint,
+        ...this.#points.slice(index + 1),
+      ];
+      this._notify(updateType, updatePoint);
+    } catch (err) {
+      throw new Error('Can\'t update point');
+    }
 
-    this._notify(updateType, update);
   }
 
   addPoint(updateType, update) {
